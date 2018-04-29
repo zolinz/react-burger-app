9740bf448a51ad0c39cf12a8de5252ec525388c2
Zoltan Kovacs <z.kovacs@leonardo.com.au>
2018-04-30 08:57:20 +1000

    added burger components lecture 114

---
+++ burger-basics-01/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react'
+import Auxi from '../../hoc/Auxi'
+
+class BurgerBuilder extends Component {
+
+    render () {
+        return (
+              <Auxi>
+                <div>Burger</div>
+                <div>Build Controls</div>
+              </Auxi>
+
+        );
+    }
+
+}
+
+export default BurgerBuilder;
